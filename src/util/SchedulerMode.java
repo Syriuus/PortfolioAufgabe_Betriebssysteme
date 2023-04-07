a612dbfc420c261c168b400058b8e7dcdede4139
Tobias <74780616+Syriuus@users.noreply.github.com>
2023-04-07 11:28:41 +0200

    Create SchedulerMode.java

---
+++ src/util/SchedulerMode.java
@@ -0,0 +1,15 @@
+package util;
+
+/*
+ * LCFS -> LastComeFirstServed.java
+ * LJF -> LongestJobFirstScheduling.java
+ * LRTF -> LongestRemainingTimeFirstScheduling.java
+ * Prio -> PrioScheduling.java
+ * RR -> RoundRobinScheduling.java
+ * SJF -> ShortestJobFirstScheduling.java 
+ */
+
+
+public enum SchedulerMode {
+	LCFS,LJF,LRTF,Prio,RR,SJF
+}
